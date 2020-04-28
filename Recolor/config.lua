aabcbee19d110e20c2fc6ba423f3ba4595be826e
Josh Beatty <joshuatb6@gmail.com>
2020-04-27 23:58:03 -0700

    Update config.lua

--- Recolor/config.lua
+++ Recolor/config.lua
@@ -2,7 +2,7 @@
 -- For more information on config.lua see the Project Configuration Guide at:
 -- https://docs.coronalabs.com/guide/basics/configSettings
 --
-
+          
 application =
 {
 	content =
